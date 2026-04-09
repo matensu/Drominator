29872d9fab454cae92e2af3f7747b83e58247400
matensu <devaucellematteo@gmail.com>
2026-04-09 15:04:23 +0200

    add: caméra

---
+++ build/CMakeFiles/minicam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minicam.dir/main.cpp.o"
+  "CMakeFiles/minicam.dir/main.cpp.o.d"
+  "minicam"
+  "minicam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minicam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
